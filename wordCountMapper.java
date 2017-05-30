551ccafd53e5f46adaa89810b9503f25b5e0cba5
akhilakandi93 <akandi7227@gmail.com>
2017-05-30 02:06:43 -0400

    Update wordCountMapper.java

--- wordCountMapper.java
+++ wordCountMapper.java
@@ -1,5 +1,29 @@
 package myPackage;
 
-public class wordCountMapper {
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class wordCountMapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable> {
+
+	@Override
+	public void map(LongWritable key, Text value,
+			OutputCollector<Text, IntWritable> output, Reporter r)
+			throws IOException {
+		String s= value.toString();
+		for(String word:s.split(" ")){
+			if(word.length()>0){
+				output.collect(new Text(word), new IntWritable(1));
+			}
+		}
+			
+		
+	}
 
 }
